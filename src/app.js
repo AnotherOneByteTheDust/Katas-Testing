33f7e323a56c251edb4f70fdb72a80301b8c56e2
Kevin Eliezer <kevineli.gp@gmail.com>
2020-10-15 21:07:55 +0100

    ADD: new kata shoplist

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-// TODO!
+import { ShopListParser } from "./io-utils/ShopListParser";
+import { ShopListReader } from "./io-utils/ShopListReader";
+import { ShopListWriter } from "./io-utils/ShopListWriter";
+import { ShopListCalculator } from "./ShopListCalculator";
+
+const shopListPath = "src/utils/assets/shopList.txt";
+const shopListOutput = "src/utils/assets/result.txt";
+
+const shopListReader = new ShopListReader(shopListPath);
+const shopListParser = new ShopListParser();
+const shopListWriter = new ShopListWriter(shopListOutput);
+
+const shopListCalculator = new ShopListCalculator(
+  shopListReader,
+  shopListParser,
+  shopListWriter,
+);
+
+shopListCalculator.calculate();
